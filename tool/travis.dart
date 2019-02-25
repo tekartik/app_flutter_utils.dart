21641d72ae2927005b14df7648d2a7f5320c3bb2
alex <alex@tekartik.com>
2019-02-25 20:14:45 +0100

    test: travis

--- tool/travis.dart
+++ tool/travis.dart
@@ -9,22 +9,16 @@ flutter doctor
   
 ''');
 
-  /*
+
   for (var dir in [
-    'app',
-    'app_emit',
-    'app_pager',
-    'app_serialize',
-    'app_mirrors'
+    'app_emit_builder',
   ]) {
     shell = shell.pushd(dir);
     await shell.run('''
     
-  pub get
   dart tool/travis.dart
   
 ''');
     shell = shell.popd();
   }
-  */
 }
