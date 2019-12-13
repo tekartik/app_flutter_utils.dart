b3d94fa0d85731b44842f40cc5ba752c35b8bbdf
alex <alex@tekartik.com>
2019-12-13 12:10:24 +0100

    fix: generated code

--- test_app/lib/generated_plugin_registrant.dart
+++ test_app/lib/generated_plugin_registrant.dart
@@ -1,13 +1,10 @@
 //
 // Generated file. Do not edit.
 //
-import 'dart:ui';
-
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:google_sign_in_web/google_sign_in_web.dart';
-
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
 
 void registerPlugins(PluginRegistry registry) {
   FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
