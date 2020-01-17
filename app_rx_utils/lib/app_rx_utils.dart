4d1d5350850ae73bd489d09cb0f2ba52fd94867e
alex <alex@tekartik.com>
2020-01-17 13:15:44 +0100

    feat: export value stream builder

--- app_rx_utils/lib/app_rx_utils.dart
+++ app_rx_utils/lib/app_rx_utils.dart
@@ -1,3 +1,4 @@
 library tekartik_app_rx_utils;
 
 export 'src/behavior_subject_builder.dart' show BehaviorSubjectBuilder;
+export 'src/value_stream_builder.dart' show ValueStreamBuilder;
--- app_rx_utils/test/app_rx_utils_test.dart
+++ app_rx_utils/test/app_rx_utils_test.dart
@@ -2,8 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:rxdart/rxdart.dart';
 
 import 'package:tekartik_app_rx_utils/app_rx_utils.dart';
-import 'package:tekartik_app_rx_utils/src/behavior_subject_builder.dart';
-import 'package:tekartik_app_rx_utils/src/value_stream_builder.dart';
 
 void main() {
   test('subject builder', () async {
