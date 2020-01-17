c5c41d2bd511886478f9b1e0fe4f14ca5027a124
alex <alex@tekartik.com>
2020-01-17 11:47:14 +0100

    feat: add value stream builder

--- app_rx_utils/test/app_rx_utils_test.dart
+++ app_rx_utils/test/app_rx_utils_test.dart
@@ -3,12 +3,23 @@ import 'package:rxdart/rxdart.dart';
 
 import 'package:tekartik_app_rx_utils/app_rx_utils.dart';
 import 'package:tekartik_app_rx_utils/src/behavior_subject_builder.dart';
+import 'package:tekartik_app_rx_utils/src/value_stream_builder.dart';
 
 void main() {
   test('subject builder', () async {
     var subject = BehaviorSubject<bool>();
     BehaviorSubjectBuilder(
       subject: subject,
+    );
+
+    await subject.close();
+  });
+
+  test('value_stream builder', () async {
+    var subject = BehaviorSubject<bool>();
+    ValueStream<bool> valueStream = subject;
+    ValueStreamBuilder(
+      stream: valueStream,
     );
 
     await subject.close();
