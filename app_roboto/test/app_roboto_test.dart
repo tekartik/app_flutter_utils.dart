5db6f8fa04b9f3f5ae69cbe1ebe27f7993cda278
alex <alex@tekartik.com>
2020-02-05 17:56:08 +0100

    feat: add roboto font package

---
+++ app_roboto/test/app_roboto_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:tekartik_app_roboto/app_roboto.dart';
+
+void main() {
+  test('adds one to input values', () {
+    expect(robotoFont, 'Roboto');
+  });
+}
