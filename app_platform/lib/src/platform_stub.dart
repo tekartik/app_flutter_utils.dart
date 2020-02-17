12a77bae829cfb365aaed9e4d5ee82be0d31a047
alex <alex@tekartik.com>
2020-02-17 11:03:03 +0100

    [platform] feat: export platform context

--- app_platform/lib/src/platform_stub.dart
+++ app_platform/lib/src/platform_stub.dart
@@ -1,5 +1,12 @@
+import 'package:tekartik_platform/context.dart';
+
+/// Support basic init for Io (Linux, Windows), skipped for Web and Mobile.
 void platformInit() => _stub('platformInit');
 
+/// Browser or io context.
+PlatformContext get platformContext => _stub('platformContext');
+
+/// Always throw.
 T _stub<T>(String message) {
   throw UnimplementedError(message);
 }
--- app_platform/test/app_platform_test.dart
+++ app_platform/test/app_platform_test.dart
@@ -1,11 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tekartik_app_platform/app_platform.dart';
+import 'package:tekartik_common_utils/common_utils_import.dart';
 
 void main() async {
   group('public', () {
     test('def', () {
       // ignore: unnecessary_statements
       platformInit;
+      // ignore: unnecessary_statements
+      platformContext;
+      // ignore: unnecessary_statements
+      PlatformContext;
+    });
+  });
+  group('platform', () {
+    test('context', () {
+      expect(platformContext, isNotNull);
+      print(jsonPretty(platformContext.toMap()));
     });
   });
 }
