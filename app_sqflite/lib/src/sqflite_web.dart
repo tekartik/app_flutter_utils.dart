7b5df6011561b6c4349258fd97224a652bee6e69
alex <alex@tekartik.com>
2020-03-13 02:43:41 +0100

    fix: build

--- app_sqflite/lib/src/sqflite_web.dart
+++ app_sqflite/lib/src/sqflite_web.dart
@@ -2,4 +2,5 @@ import 'package:sqflite_common/sqlite_api.dart';
 
 DatabaseFactory get databaseFactory => null;
 
-Future<DatabaseFactory> getDatabaseFactory(String packageName) async => databaseFactory;
+Future<DatabaseFactory> initDatabaseFactory(String packageName) async =>
+    databaseFactory;
