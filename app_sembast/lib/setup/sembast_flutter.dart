85a3b2f17cb20124ef6159d3421695f2a466d8ae
alex <alex@tekartik.com>
2020-04-09 18:33:59 +0200

    feat: restore deprecated setup for compatibility

---
+++ app_sembast/lib/setup/sembast_flutter.dart
@@ -0,0 +1,18 @@
+import 'package:sembast/sembast.dart';
+import 'package:tekartik_app_flutter_sembast/sembast.dart';
+
+export 'package:sembast/sembast.dart';
+
+/// Opinionated factory for flutter
+///
+/// (app_dir)/db on iOS/Android or when getApplicationDocumentsDirectory is supported
+///
+/// (app_data)/<package_name>/db on Desktop
+///
+/// IndexedDB db for the web
+///
+/// [packageName] only needed for desktop.
+@Deprecated('User getDatabaseFactory instead')
+Future<DatabaseFactory> initDatabaseFactory({String packageName}) async {
+  return getDatabaseFactory(packageName: packageName);
+}
