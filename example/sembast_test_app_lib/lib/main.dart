1566e88264146a436809f84d130ef620dbe76bca
alex <alex@tekartik.com>
2020-04-09 18:33:59 +0200

    feat: restore deprecated setup for compatibility

--- example/sembast_test_app_lib/lib/main.dart
+++ example/sembast_test_app_lib/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:tekartik_app_platform/app_platform.dart';
 import 'package:tekartik_app_flutter_sembast/sembast.dart';
 import 'package:tekartik_app_flutter_sembast/setup/sembast_flutter.dart';
+import 'package:tekartik_app_platform/app_platform.dart';
 import 'package:tekartik_test_menu_flutter/test.dart';
 import 'package:tekartik_test_menu_flutter/test_menu_flutter.dart';
 
@@ -8,8 +8,8 @@ void defineMenu() {
   menu('sembast', () {
     //devPrint('MAIN_');
     item('open and toggle value', () async {
-      var prefsFactory = await initDatabaseFactory(
-          packageName: 'app_sembast_test_app.tekartik.com');
+      var prefsFactory =
+          getDatabaseFactory(packageName: 'app_sembast_test_app.tekartik.com');
       var store = StoreRef<String, bool>.main();
       var prefs = await prefsFactory.openDatabase('open_toggle_sembast.db');
       var toggle = await store.record('toggle').get(prefs);
