ac195575172c92725b7b6972b284341ad1474f0c
Alexandre Roux <alex@tekartik.com>
2020-07-01 21:15:28 +0200

    feat: support sqflite on windows

--- app_sembast/test/sembast_sqflite_test.dart
+++ app_sembast/test/sembast_sqflite_test.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:tekartik_app_flutter_sembast/setup/sembast_sqflite_ffi.dart';
+import 'package:tekartik_app_flutter_sqflite/sqflite.dart';
 
 Future main() async {
   sqfliteFfiInit();
   group('sembast', () {
     test('factory', () async {
+      sqfliteWindowsFfiInit();
       var factory = databaseFactorySqfliteFfi;
       var db = await factory.openDatabase('test.db');
 
