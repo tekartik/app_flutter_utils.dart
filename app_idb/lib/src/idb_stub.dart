1d81b546d7096b090c76ec7e1a3df7b13b127156
alex <alex@tekartik.com>
2020-10-28 23:02:57 +0100

    feat: new getIdbFactoryApi (sorry...)

--- app_idb/lib/src/idb_stub.dart
+++ app_idb/lib/src/idb_stub.dart
@@ -1,7 +1,7 @@
 import 'package:idb_shim/idb_shim.dart';
 
 IdbFactory get idbFactory => _stub('idbFactory');
-IdbFactory getIdbFactory(String packageName) =>
+IdbFactory getIdbFactory({String packageName}) =>
     _stub('getIdbFactory($packageName)');
 
 T _stub<T>(String message) {
