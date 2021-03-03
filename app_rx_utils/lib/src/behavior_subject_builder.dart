9b8a0c7544ae90a7060bf19d403c0c6aaa18e876
alex <alex@tekartik.com>
2021-03-03 23:48:01 +0100

    app_rx_utils: null_safety support

--- app_rx_utils/lib/src/behavior_subject_builder.dart
+++ app_rx_utils/lib/src/behavior_subject_builder.dart
@@ -5,7 +5,7 @@ class BehaviorSubjectBuilder<T> extends StatelessWidget {
   final BehaviorSubject<T> subject;
   final AsyncWidgetBuilder<T> builder;
 
-  const BehaviorSubjectBuilder({Key key, this.subject, this.builder})
+  const BehaviorSubjectBuilder({Key? key, required this.subject, required this.builder})
       : super(key: key);
 
   @override
--- app_rx_utils/lib/src/value_stream_builder.dart
+++ app_rx_utils/lib/src/value_stream_builder.dart
@@ -5,7 +5,7 @@ class ValueStreamBuilder<T> extends StatelessWidget {
   final ValueStream<T> stream;
   final AsyncWidgetBuilder<T> builder;
 
-  const ValueStreamBuilder({Key key, this.stream, this.builder})
+  const ValueStreamBuilder({Key? key, required this.stream, required this.builder})
       : super(key: key);
 
   @override
