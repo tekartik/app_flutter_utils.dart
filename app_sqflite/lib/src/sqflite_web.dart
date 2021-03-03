453e2492a255392befdb77a94e1d983da87de114
alex <alex@tekartik.com>
2021-03-03 23:25:38 +0100

    app_sqflite: null_safety support

--- app_sqflite/lib/src/sqflite_web.dart
+++ app_sqflite/lib/src/sqflite_web.dart
@@ -1,11 +1,11 @@
 import 'package:sqflite_common/sqlite_api.dart';
 
-DatabaseFactory get databaseFactory => null;
+DatabaseFactory? get databaseFactory => null;
 
-Future<DatabaseFactory> initDatabaseFactory(String packageName) async =>
+Future<DatabaseFactory?> initDatabaseFactory(String packageName) async =>
     databaseFactory;
 
-DatabaseFactory getDatabaseFactory({String packageName, String rootPath}) =>
+DatabaseFactory? getDatabaseFactory({String? packageName, String? rootPath}) =>
     databaseFactory;
 
 /// Only needed/implemented on windows
