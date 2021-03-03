2bb843abb2231b1a483cd98ff1d290a285b15e8d
alex <alex@tekartik.com>
2021-03-04 00:48:54 +0100

    fix format

--- app_rx_utils/lib/src/behavior_subject_builder.dart
+++ app_rx_utils/lib/src/behavior_subject_builder.dart
@@ -5,7 +5,8 @@ class BehaviorSubjectBuilder<T> extends StatelessWidget {
   final BehaviorSubject<T> subject;
   final AsyncWidgetBuilder<T> builder;
 
-  const BehaviorSubjectBuilder({Key? key, required this.subject, required this.builder})
+  const BehaviorSubjectBuilder(
+      {Key? key, required this.subject, required this.builder})
       : super(key: key);
 
   @override
