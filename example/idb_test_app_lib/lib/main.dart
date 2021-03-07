4cb575c8e9b4238e10e3aa24f44c30ac2f37db43
alex <alex@tekartik.com>
2021-03-07 16:35:19 +0100

    fix: sqflite web compilation

--- example/idb_test_app_lib/lib/main.dart
+++ example/idb_test_app_lib/lib/main.dart
@@ -5,25 +5,28 @@ import 'package:tekartik_test_menu_flutter/test_menu_flutter.dart';
 
 void defineMenu() {
   menu('idb', () {
-    //devPrint('MAIN_');
+    var idbFactory =
+        getIdbFactory(packageName: 'app_idb_test_app.tekartik.com');
+    var dbName = 'open_toggle_idb.db';
+    item('delete database', () async {
+      await idbFactory.deleteDatabase(dbName);
+    });
     item('open and toggle value', () async {
-      var prefsFactory =
-          getIdbFactory(packageName: 'app_idb_test_app.tekartik.com');
-      var prefs = await prefsFactory.open('open_toggle_idb.db', version: 1,
-          onUpgradeNeeded: (event) {
+      var db =
+          await idbFactory.open(dbName, version: 1, onUpgradeNeeded: (event) {
         var db = event.database;
         db.createObjectStore('main');
       });
-      var toggle = await prefs
+      var toggle = await db
           .transaction('main', idbModeReadOnly)
           .objectStore('main')
           .getObject('toggle') as bool;
       toggle = !(toggle ?? false);
-      await prefs
+      await db
           .transaction('main', idbModeReadWrite)
           .objectStore('main')
           .put(toggle, 'toggle');
-      prefs.close();
+      db.close();
       write('set toogle to $toggle');
     });
   });
--- example/sembast_test_app_lib/lib/main.dart
+++ example/sembast_test_app_lib/lib/main.dart
@@ -6,12 +6,15 @@ import 'package:tekartik_test_menu_flutter/test_menu_flutter.dart';
 
 void defineMenu() {
   menu('sembast', () {
-    //devPrint('MAIN_');
+    var sembastFactory =
+        getDatabaseFactory(packageName: 'app_sembast_test_app.tekartik.com');
+    var dbName = 'open_toggle_sembast.db';
+    item('delete database', () async {
+      await sembastFactory.deleteDatabase(dbName);
+    });
     item('open and toggle value', () async {
-      var prefsFactory =
-          getDatabaseFactory(packageName: 'app_sembast_test_app.tekartik.com');
       var store = StoreRef<String, bool>.main();
-      var prefs = await prefsFactory.openDatabase('open_toggle_sembast.db');
+      var prefs = await sembastFactory.openDatabase(dbName);
       var toggle = await store.record('toggle').get(prefs);
       toggle = !(toggle ?? false);
       await store.record('toggle').put(prefs, toggle);
