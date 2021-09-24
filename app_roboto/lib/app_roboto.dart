304d1aefc693e6657d5cb64f409fa901e021b8ea
alex <alex@tekartik.com>
2021-09-24 15:37:56 +0200

    feat: roboto mono font

--- app_roboto/lib/app_roboto.dart
+++ app_roboto/lib/app_roboto.dart
@@ -1,1 +1,8 @@
-const robotoFont = 'Roboto';
+// To deprecate
+const robotoFont = robotoFontFamily;
+
+/// Roboto font.
+const robotoFontFamily = 'Roboto';
+
+/// Roboto mono font.
+const robotoMonoFontFamily = 'RobotoMono';
