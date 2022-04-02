775352788d4c44b67fe55d6ddfb5c91e320c05f6
Alexandre Roux <alex@tekartik.com>
2022-04-02 12:05:56 +0200

    obsolete: remove compatSetDatabasesPath

--- app_sqflite/lib/src/sqflite_io.dart
+++ app_sqflite/lib/src/sqflite_io.dart
@@ -6,8 +6,6 @@ import 'package:sqflite/sqflite.dart' as sqflite show databaseFactory;
 import 'package:sqflite/sqlite_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-import 'sqflite_import.dart';
-
 String buildDatabasesPath(String packageName) {
   var dataPath = join(userAppDataPath, packageName, 'databases');
   try {
@@ -34,8 +32,7 @@ DatabaseFactory get databaseFactory => _defaultDatabaseFactory;
 Future<DatabaseFactory> initDatabaseFactory(String packageName) async {
   if (Platform.isLinux || Platform.isWindows) {
     var databaseFactory = databaseFactoryFfi;
-    await databaseFactory
-        .compatSetDatabasesPath(buildDatabasesPath(packageName));
+    await databaseFactory.setDatabasesPath(buildDatabasesPath(packageName));
     return databaseFactory;
   } else {
     return databaseFactory;
@@ -47,8 +44,8 @@ DatabaseFactory getDatabaseFactory({String? packageName, String? rootPath}) {
   if (Platform.isLinux || Platform.isWindows) {
     var databaseFactory = databaseFactoryFfi;
     // Should not return a future...or ignore
-    databaseFactory.compatSetDatabasesPath(
-        rootPath ?? buildDatabasesPath(packageName ?? '.'));
+    databaseFactory
+        .setDatabasesPath(rootPath ?? buildDatabasesPath(packageName ?? '.'));
     return databaseFactory;
   } else {
     return databaseFactory;
