53a8cd55189ea5d81e54a6f1620e4e949afdbc21
Alexandre Roux <alex@tekartik.com>
2022-04-02 22:04:03 +0200

    feat: add platform helpers

---
+++ app_platform/lib/platform.dart
@@ -0,0 +1,20 @@
+/// Do not include with flutter_test. or add suffix.
+import 'package:tekartik_app_platform/app_platform.dart';
+
+/// Safe (false) on the web
+final isIOS = platformContext.io?.isIOS ?? false;
+
+/// Safe (false) on the web
+final isAndroid = platformContext.io?.isAndroid ?? false;
+
+/// Safe (false) on the web
+final isLinux = platformContext.io?.isLinux ?? false;
+
+/// Safe (false) on the web
+final isWindows = platformContext.io?.isWindows ?? false;
+
+/// Safe (false) on the web
+final isMacOS = platformContext.io?.isMacOS ?? false;
+
+/// Safe (false) on io
+final isWeb = platformContext.browser != null;
---
+++ app_platform/test/platform_io_test.dart
@@ -0,0 +1,27 @@
+@TestOn('vm')
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tekartik_app_platform/app_platform.dart';
+import 'package:tekartik_app_platform/platform.dart' as p;
+import 'package:tekartik_common_utils/common_utils_import.dart';
+
+void main() async {
+  group('platform_io', () {
+    test('isXxx vs flutter test', () {});
+    test('isXxx', () {
+      if (Platform.isMacOS) {
+        expect(p.isMacOS, isTrue);
+      } else if (Platform.isLinux) {
+        expect(p.isMacOS, isFalse);
+        expect(p.isLinux, isTrue);
+        expect(p.isWindows, isFalse);
+      } else if (Platform.isWindows) {
+        expect(p.isMacOS, isFalse);
+        expect(p.isLinux, isFalse);
+        expect(p.isWindows, isTrue);
+      }
+      print(jsonPretty(platformContext.toMap())); // ignore: avoid_print
+    });
+  });
+}
