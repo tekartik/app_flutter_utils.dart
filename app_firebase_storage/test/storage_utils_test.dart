71005cf650d83783881a5620335ae603752bbe9d
Alexandre Roux <alex@tekartik.com>
2022-10-02 10:32:14 +0200

    feat: app firebase storage utils

---
+++ app_firebase_storage/test/storage_utils_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tekartik_app_flutter_firebase_storage/storage_utils.dart';
+
+void main() {
+  group('storage_utils', () {
+    test('firebaseStorageGetAdminWebUploadFolder', () {
+      expect(firebaseStorageGetAdminWebUploadFolder('1', '2', '3'),
+          'https://console.firebase.google.com/project/1/storage/2/files/3');
+    });
+  });
+}
