02dc21e40a5f3e65c55d5bd68b173df82a44b216
Alexandre Roux <alex@tekartik.com>
2022-10-16 22:54:54 +0200

    [app_sqflite] init web

--- app_sqflite/lib/src/sqflite_web.dart
+++ app_sqflite/lib/src/sqflite_web.dart
@@ -1,6 +1,8 @@
 import 'package:sqflite_common/sqlite_api.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
-DatabaseFactory get databaseFactory => _stub('databaseFactory');
+/// Web factory is fined by default.
+DatabaseFactory get databaseFactory => databaseFactoryFfiWeb;
 
 Future<DatabaseFactory> initDatabaseFactory(String packageName) async =>
     databaseFactory;
