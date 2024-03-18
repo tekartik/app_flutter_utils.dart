106770cad3f8a52966c6e0b2e4b94d9ea78e0722
Alexandre Roux <alex@tekartik.com>
2024-03-18 18:32:47 +0100

    fix: unit test

--- repo_support/test/gen_test.dart
+++ repo_support/test/gen_test.dart
@@ -23,6 +23,7 @@ void main() {
           'sqflite_test_app_lib',
           'widget_test_app_lib',
           'common_test_app_lib',
+          'common_web_test_app_lib',
           'app_plugin_test_app_lib',
         ]) {
           await copyDirectory(Directory(join(topDir, 'example', dir)),
