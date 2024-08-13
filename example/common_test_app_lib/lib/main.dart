e0422208b759ef5859bc6444ea75214cb43c624c
Alexandre Roux <alex@tekartik.com>
2024-08-14 01:34:19 +0200

    fix sqflite web

--- example/common_test_app_lib/lib/main.dart
+++ example/common_test_app_lib/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:tekartik_app_flutter_common_utils/asset/asset_utils.dart';
+import 'package:tekartik_app_flutter_common_utils/common_utils_import.dart';
 import 'package:tekartik_test_menu_flutter/test.dart';
 
 void defineMenu() {
   menu('common', () {
+    item('debugEnvMap', () {
+      write(jsonPretty(debugEnvMap));
+    });
     item('list assets', () async {
       for (var asset in await getAssetList()) {
         write(asset);
