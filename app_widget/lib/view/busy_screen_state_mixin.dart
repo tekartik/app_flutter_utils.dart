210ba2cbe4f5363273add87c9b1c477b0c1752c9
Alexandre Roux <alex@tekartik.com>
2024-09-26 11:41:54 +0200

    fix lints

--- app_widget/lib/view/busy_screen_state_mixin.dart
+++ app_widget/lib/view/busy_screen_state_mixin.dart
@@ -18,11 +18,11 @@ mixin BusyScreenStateMixin<T extends StatefulWidget> on State<T> {
   final _busySubject = BehaviorSubject<bool>.seeded(false);
   ValueStream<bool> get busyStream => _busySubject.stream;
 
-  bool get busy => _busySubject.value == true;
+  bool get busy => _busySubject.value;
 
   /// Action if not busy
   Future<BusyActionResult<R>> busyAction<R>(Future<R> Function() action) async {
-    if (_busySubject.value == true) {
+    if (_busySubject.value) {
       return BusyActionResult(busy: true);
     }
     _busySubject.add(true);
