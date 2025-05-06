0526d0e23bfcdebfb71f31ad23d6a56bce6cc179
Alexandre Roux <alex@tekartik.com>
2025-05-06 09:26:58 +0200

    hide obsolete logs helper

--- app_common/lib/common_utils_import.dart
+++ app_common/lib/common_utils_import.dart
@@ -1,1 +1,12 @@
-export 'package:tekartik_common_utils/common_utils_import.dart';
+export 'package:tekartik_common_utils/common_utils_import.dart'
+    hide
+        log,
+        LogRecord,
+        Logger,
+        Level,
+        setupQuickLogging,
+        debugQuickLogging,
+        logLevels,
+        parseLogLevel,
+        format0To1AsPercent,
+        formatTimestampMs;
