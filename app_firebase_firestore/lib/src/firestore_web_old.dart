b1b5898016bd5e96144d764145e202b5e40cba33
Alexandre Roux <alex@tekartik.com>
2025-06-27 17:56:02 +0200

    remove old firebase browser context

--- app_firebase_firestore/lib/src/firestore_web_old.dart
+++
@@ -1,8 +0,0 @@
-@Deprecated('User authServiceFlutter instead')
-library;
-
-import 'package:tekartik_firebase_firestore_browser/firestore_browser.dart';
-
-import 'import.dart';
-
-FirestoreService get firestoreService => firestoreServiceBrowser;
