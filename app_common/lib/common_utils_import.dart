b7d1ad0faf4ed56e2ee71c40d79cee81f0bc6140
Alexandre Roux <alex@tekartik.com>
2025-08-01 22:18:34 +0200

    fix import

--- app_common/lib/common_utils_import.dart
+++ app_common/lib/common_utils_import.dart
@@ -1,12 +1,2 @@
 export 'package:tekartik_common_utils/common_utils_import.dart'
-    hide
-        log,
-        LogRecord,
-        Logger,
-        Level,
-        setupQuickLogging,
-        debugQuickLogging,
-        logLevels,
-        parseLogLevel,
-        format0To1AsPercent,
-        formatTimestampMs;
+    hide format0To1AsPercent, formatTimestampMs;
