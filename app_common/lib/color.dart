e12c5bb4e9b12f8e2c4223f17933509be6678d56
Alexandre Roux <alex@tekartik.com>
2025-09-09 15:35:47 +0200

    feat: add color helper

--- app_common/lib/color.dart
+++ app_common/lib/color.dart
@@ -1,5 +1,5 @@
-export 'src/color.dart' show generateMaterialColor;
-export 'src/color_compat.dart' show TekartikColorFlutterExt;
+export 'src/color.dart' show generateMaterialColor, TekartikColorFlutterExt;
+export 'src/color_compat.dart' show TekartikColorFlutterCompatExt;
 export 'src/material_color_compat.dart'
     show
         generateMaterialColorCompat,
