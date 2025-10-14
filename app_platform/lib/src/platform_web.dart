5e8852381f5bdf399f8e53b42915a7bd649e5be9
Alexandre Roux <alex@tekartik.com>
2025-10-14 14:52:42 +0200

    fix lints

--- app_platform/lib/src/platform_web.dart
+++ app_platform/lib/src/platform_web.dart
@@ -1,8 +1,6 @@
 // Noop
 import 'package:tekartik_platform_browser/context_browser.dart';
 
-import 'import.dart' show PlatformContext;
-
 void platformInit() {}
 
 PlatformContext get platformContext => platformContextBrowser;
